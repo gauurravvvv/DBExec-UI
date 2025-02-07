e1738052fa25aa2928a96d41a35a04427b08859b
gaurav.goel <gaurav.goel@ultragenicglobal.com>
2025-02-07 19:11:14 +0530

    header fixed

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Router } from "@angular/router";
-import { GlobalService } from "src/app/core/services/global.service";
-import { interval, Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalService } from 'src/app/core/services/global.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   currentTime: Date = new Date();
-  organisationName: string = "";
-  userInitials: string = "";
-  userName: string = "";
-  userRole: string = "";
+  organisationName: string = '';
+  userInitials: string = '';
+  userName: string = '';
+  userRole: string = '';
   private timeSubscription?: Subscription;
+  showProfileMenu: boolean = false;
 
   constructor(private router: Router, private globalService: GlobalService) {}
 
   ngOnInit() {
     // Get organization name
     this.organisationName =
-      this.globalService.getTokenDetails("organisationName");
+      this.globalService.getTokenDetails('organisationName');
 
     // Get user details and create initials
-    const userFullName = this.globalService.getTokenDetails("name");
+    const userFullName = this.globalService.getTokenDetails('name');
     this.userName = userFullName;
     this.userInitials = this.globalService.chipNameProvider(userFullName);
-    this.userRole = this.globalService.getTokenDetails("role");
+    this.userRole = this.globalService.getTokenDetails('role');
 
     // Update time every second
     this.timeSubscription = interval(1000).subscribe(() => {
@@ -43,6 +44,24 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   logout() {
     localStorage.clear();
-    this.router.navigate(["/login"]);
+    this.router.navigate(['/login']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    const userProfile = document.querySelector('.user-profile');
+    if (!userProfile?.contains(event.target)) {
+      this.showProfileMenu = false;
+    }
+  }
+
+  toggleProfileMenu(event: Event) {
+    event.stopPropagation();
+    this.showProfileMenu = !this.showProfileMenu;
+  }
+
+  viewProfile() {
+    // Implement profile view logic
+    this.showProfileMenu = false;
   }
 }
