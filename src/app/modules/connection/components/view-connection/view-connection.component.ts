0b495ff6377e55f74e8e7a25b033d924b653f6fc
gaurav.goel <gaurav.goel@ultragenicglobal.com>
2026-04-24 19:30:02 +0530

    refactor(connection): signal store, takeUntilDestroyed, fix OnPush
    - ConnectionService: add signal store (_connections, _total, _current, _loading, _saving) with load/loadOne/add/update/delete/bulkDelete async methods; keep legacy listConnection/viewConnection for external callers
    - list-connection: remove destroy$/ngOnDestroy, use DestroyRef+takeUntilDestroyed; drop local connections/filteredConnections/totalRecords, bind template to connectionService.connections()/total(); async loadConnections/proceedDelete with markForCheck
    - add-connection: remove destroy$/ngOnDestroy, use DestroyRef+takeUntilDestroyed; expose saving signal; async onSubmit; markForCheck after async loads
    - edit-connection: remove destroy$/ngOnDestroy, use DestroyRef+takeUntilDestroyed; expose saving signal; loadConnectionData via loadOne()+current(); async proceedSave; markForCheck after all async mutations
    - view-connection: async loadConnectionDetails via loadOne()+current(); async proceedDelete; markForCheck after all async data loads

--- src/app/modules/connection/components/view-connection/view-connection.component.ts
+++ src/app/modules/connection/components/view-connection/view-connection.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CONNECTION } from 'src/app/constants/routes';
 import { GlobalService } from 'src/app/core/services/global.service';
 import { ConnectionService } from '../../services/connection.service';
+
 @Component({
   selector: 'app-view-connection',
   templateUrl: './view-connection.component.html',
@@ -21,6 +22,7 @@ export class ViewConnectionComponent implements OnInit {
     private router: Router,
     private globalService: GlobalService,
     private connectionService: ConnectionService,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit() {
@@ -29,14 +31,13 @@ export class ViewConnectionComponent implements OnInit {
     this.loadConnectionDetails();
   }
 
-  loadConnectionDetails() {
-    this.connectionService
-      .viewConnection(this.orgId, this.connectionId)
-      .then(response => {
-        if (this.globalService.handleSuccessService(response, false)) {
-          this.connectionData = response.data;
-        }
-      });
+  async loadConnectionDetails() {
+    await this.connectionService.loadOne(this.orgId, this.connectionId);
+    const data = this.connectionService.current();
+    if (data) {
+      this.connectionData = data;
+    }
+    this.cdr.markForCheck();
   }
 
   onEdit() {
@@ -64,21 +65,19 @@ export class ViewConnectionComponent implements OnInit {
     this.deleteJustification = '';
   }
 
-  proceedDelete(): void {
+  async proceedDelete(): Promise<void> {
     if (this.connectionData && this.deleteJustification.trim()) {
-      this.connectionService
-        .deleteConnection(
-          this.orgId,
-          this.connectionData.id,
-          this.deleteJustification.trim(),
-        )
-        .then(response => {
-          this.showDeleteConfirm = false;
-          this.deleteJustification = '';
-          if (this.globalService.handleSuccessService(response)) {
-            this.router.navigate([CONNECTION.LIST]);
-          }
-        });
+      const response = await this.connectionService.delete(
+        this.orgId,
+        this.connectionData.id,
+        this.deleteJustification.trim(),
+      );
+      this.showDeleteConfirm = false;
+      this.deleteJustification = '';
+      if (this.globalService.handleSuccessService(response)) {
+        this.router.navigate([CONNECTION.LIST]);
+      }
+      this.cdr.markForCheck();
     }
   }
 }
